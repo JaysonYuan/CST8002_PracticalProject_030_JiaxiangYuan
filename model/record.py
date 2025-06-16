2ef257df722bbb2f2db4a39686ab9110ec5ef7c4
JaysonYuan <yuan0080@algonquinlive.com>
2025-06-15 21:30:15 -0400

    Add Record model representing Sr-90 dataset columns

--- model/record.py
+++ model/record.py
@@ -9,13 +9,13 @@ class Record:
     def __init__(self, sample_type, type_, start_date, stop_date,
                  station_name, province, sr90_activity,
                  sr90_error, sr90_calcium_activity):
-        self.sample_type = sample_type  # Sample Type/ Type d'échantillon
-        self.type_ = type_              # Type
-        self.start_date = start_date    # Start Date/ Date de Début
-        self.stop_date = stop_date      # Stop Date/ Date de Fin
-        self.station_name = station_name  # Station Name/ Nom de Station
-        self.province = province        # Province
-        self.sr90_activity = sr90_activity  # Sr90 Activity/ Activité (Bq/L)
+        self.sample_type = sample_type
+        self.type_ = type_
+        self.start_date = start_date
+        self.stop_date = stop_date
+        self.station_name = station_name
+        self.province = province
+        self.sr90_activity = sr90_activity
         self.sr90_error = sr90_error
         self.sr90_calcium_activity = sr90_calcium_activity
 
