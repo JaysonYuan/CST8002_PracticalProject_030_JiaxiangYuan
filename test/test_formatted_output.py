72380691b4d945d38cea4118f799cef22f471c3c
JaysonYuan <yuan0080@algonquinlive.com>
2025-07-13 17:06:37 -0400

    Add unit tests for FormattedRecordA and FormattedRecordB display methods to verify polymorphic output formats

--- test/test_formatted_output.py
+++ test/test_formatted_output.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import unittest
 from model.formatted_record_a import FormattedRecordA
 from model.formatted_record_b import FormattedRecordB
