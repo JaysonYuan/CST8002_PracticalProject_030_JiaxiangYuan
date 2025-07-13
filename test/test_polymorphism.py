528f686816648fa4568e17d359fa13176e4ee70f
JaysonYuan <yuan0080@algonquinlive.com>
2025-07-13 18:07:02 -0400

    Add unit tests for FormattedRecordA and FormattedRecordB display methods with author info

--- test/test_polymorphism.py
+++ test/test_polymorphism.py
@@ -1,5 +1,5 @@
 """
-Unit test for polymorphic record formatting.
+Unit test for FormattedRecordA and FormattedRecordB classes.
 
 Author: Jiaxiang Yuan
 """
@@ -14,11 +14,17 @@ from model.formatted_record_b import FormattedRecordB
 class TestPolymorphicRecords(unittest.TestCase):
 
     def test_dash_format(self):
+        """
+        Test FormattedRecordA display method.
+        """ 
         rec = FormattedRecordA("001", "Calgary-AB-01-Jan-84", 0.105)
         expected = "001 - Calgary-AB-01-Jan-84 - 0.105"
         self.assertEqual(rec.display(), expected)
 
     def test_label_format(self):
+        """
+        Test FormattedRecordB display method.
+        """
         rec = FormattedRecordB("002", "Calgary-AB-01-Apr-84", 0.0628)
         expected = "ID: 002\nName: Calgary-AB-01-Apr-84\nValue: 0.0628"
         self.assertEqual(rec.display(), expected)
